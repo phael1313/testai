fac79667532c68c17a0bf380c3cc35611b94cc51
phael1313 <raphaelmorais.net@gmail.com>
2025-04-22 20:22:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def upload_file():
     return jsonify({"html": html_result})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
